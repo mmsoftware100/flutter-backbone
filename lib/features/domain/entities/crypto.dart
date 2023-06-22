623b34b0e702731ea1836d11040dc22d95879577
Aung Ko Man <aungkoman@gmail.com>
2023-06-22 09:08:30 +0630

    crypto entity added

---
+++ lib/features/domain/entities/crypto.dart
@@ -0,0 +1,16 @@
+class Crypto {
+  int id;
+  String name;
+  String symbol;
+  double price;
+  double marketCap;
+
+  Crypto(
+      { required this.id,
+        required this.name,
+        required this.symbol,
+        required this.price,
+        required this.marketCap
+      });
+
+}

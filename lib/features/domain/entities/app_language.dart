f4c22d64f02ee6e48274d291265db24eb8c85fd3
Aung Ko Man <aungkoman@gmail.com>
2023-07-18 09:27:55 +0630

    language provider added

---
+++ lib/features/domain/entities/app_language.dart
@@ -0,0 +1,15 @@
+class AppLanguage{
+  String name;
+  String code;
+  String flag;
+
+  AppLanguage({
+    required this.name,
+    required this.code,
+    required this.flag
+});
+}
+
+// https://fumoinvest.org/profit_calculator
+// https://fumoinvest.org/profit_calculator/ch
+// https://fumoinvest.org/profit_calculator/jp
---
+++ lib/features/presentation/providers/language_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../domain/entities/app_language.dart';
+
+class LanguageProvider extends ChangeNotifier{
+  List<AppLanguage> appLanguageList = [
+    AppLanguage(name: "English", code: "en", flag: "https://fumoinvest.org/img/eng_flag.png"),
+    AppLanguage(name: "China", code: "ch", flag: "https://fumoinvest.org/img/ch_flag.png"),
+    AppLanguage(name: "Japan", code: "jp", flag: "https://fumoinvest.org/img/jp_flag.png"),
+  ];
+}
